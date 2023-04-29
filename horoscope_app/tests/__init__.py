b3480998f58216f26f8b76d00dd421b296ebfb99
Alexandr <maximchik.alexandr@yandex.ru>
2023-04-29 19:59:44 +0300

    Add tests of the views

--- horoscope_app/tests/__init__.py
+++ horoscope_app/tests/__init__.py
@@ -1,6 +1,15 @@
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
+ZODIACS = (
+    ("aries", "Овен"),
+    ("taurus", "Телец"),
+    ("gemini", "Близнецы"),
+    ("cancer", "Рак"),
+    ("leo", "Лев"),
+    ("virgo", "Дева"),
+    ("libra", "Весы"),
+    ("scorpio", "Скорпион"),
+    ("sagittarius", "Стрелец"),
+    ("capricorn", "Козерог"),
+    ("aquarius", "Водолей"),
+    ("pisces", "Рыбы"),
+)
+ZODIAC_FIXTURE = "horoscope_app/migrations/fixtures/dump_db.json"
